f38bfbe8ba4e46736e3e48b72e440fc334c724b3
Bright Nimoh Boakye <brightnimohboakye@gmail.com>
2026-06-21 20:26:09 +0000

    Register the Post model with Django admin and customize the admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from blog.models import Post
+
+# Define a custom interface for the Post model
+class PostAdmin(admin.ModelAdmin):
+    # Determine which database columns are visible in the list view
+    list_display = ('title', 'author', 'category', 'created_at')
+
+    # Enable a search bar that queries these fields
+    search_fields = ('title', 'body')
+
+    # Automatically generate the slug in the form as you type the title [3]
+    prepopulated_fields = {'slug': ('title',)}
+
+# Register the model along with its custom PostAdmin interface
+admin.site.register(Post, PostAdmin)
